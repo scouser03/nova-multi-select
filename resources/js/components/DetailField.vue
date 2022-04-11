24430cb46fe7498999ba2e88a223cf2f2a51e2f2
Khvicha Ostatishvili <khvicha.ostatishvili@ens.tsu.edu.ge>
2022-04-12 01:18:40 +0400

    fix: remove index for resource  detail page

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -10,7 +10,7 @@
         <p class="text-90">
             <div v-if="data.length" class="flex items-center space-2">
                 <div v-for="(item, index) in data" :key="item.id" style="background-color:#4099de" class="relative group overflow-hidden px-2 py-1 rounded-lg flex items-center mr-2">
-                    <span class="relative btn text-white">{{  item[name] }} {{ index }}</span>
+                    <span class="relative btn text-white">{{  item[name] }}</span>
                     <div class="flex items-center">
                         <svg @click="remove(item)" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-2 font-bold cursor-pointer hover:text-white" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
